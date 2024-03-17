1454a04843e8a6acafe58b5c1d16b562f653f5c6
Viet Dang <vietdanghoang1705@gmail.com>
2024-03-17 21:22:17 +0700

    create categories filter, bottom sheet

--- app/(user)/(tabs)/home.jsx
+++ app/(user)/(tabs)/home.jsx
@@ -1,9 +1,24 @@
-import { Text, View } from "react-native"
+import { useRef } from "react";
+import { Text, View, Button } from "react-native"
+import BottomSheet from "../../../components/BottomSheet";
+import CategoriesFilter from "../../../components/CategoriesFilter";
 
 const Home = () => {
+  const bottomSheetRef = useRef(null);
+
+  const openBottomSheet = () => {
+   
+    bottomSheetRef.current?.expand();
+  }
   return (
-    <View className="flex-1 justify-center items-center bg-red-300">
+    <View className="flex-1">
         <Text className="text-center">Home page</Text>
+        <CategoriesFilter />
+        <Button title="open bottom sheet" onPress={openBottomSheet}/>
+
+        <BottomSheet ref={bottomSheetRef}>
+        <Text>Hi</Text>
+      </BottomSheet>
     </View>
   )
 }
---
+++ components/BottomSheet/index.jsx
@@ -0,0 +1,27 @@
+import { View } from 'react-native';
+import React, { forwardRef, useMemo } from 'react';
+import GorhomeBottomSheet  from '@gorhom/bottom-sheet';
+
+const BottomSheet = forwardRef((props, ref) => {
+	const snapPoints = useMemo(() => ['50%', '70%', '90%'], []);
+
+    const initialIndex = props.initialIndex !== undefined ? props.initialIndex : -1;
+
+	return (
+		<GorhomeBottomSheet
+			ref={ref}
+			index={initialIndex}
+			snapPoints={snapPoints}
+			enablePanDownToClose={true}
+			handleIndicatorStyle={{ backgroundColor: '#ccc' }}
+			backgroundStyle={{ backgroundColor: 'white' }}
+		>
+			<View className='flex bg-white'>
+				{props.children}
+			</View>
+		</GorhomeBottomSheet>
+	);
+});
+
+
+export default BottomSheet;
